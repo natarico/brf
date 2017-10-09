76c1d7cb8cf3da01824856562c5b93be464c5395
Natalia <nataliae.dev@gmail.com>
2017-10-09 14:04:33 -0700

    First unit test

--- natari/natari/urls.py
+++ natari/natari/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from apps.brf import views
 
 urlpatterns = [
+    url(r'^$', views.home_page),
     url(r'^admin/', admin.site.urls),
 ]
